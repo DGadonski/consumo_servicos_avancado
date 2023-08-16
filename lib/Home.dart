5238a9dc36d927a9af721eec2ce2afe33915adb7
DGadonski <derek.gadonski@gmail.com>
2023-08-16 16:34:54 -0300

    Future e ListView.builder

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,60 +1,85 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: prefer_final_fields, use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors, file_names
 
+import 'package:consumo_servicos_avancado/Post.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
-
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
 
-  Future<Map> _recuperarPreco() async{
-    String url = 'https://blockchain.info/ticker';
-    http.Response response = await http.get(Uri.parse(url));
-    return jsonDecode(response.body);
+  String _urlBase = "https://jsonplaceholder.typicode.com/posts";
+
+  Future<List<Post>> _recuperarPostagens() async {
+    
+    http.Response response = await http.get(Uri.parse(_urlBase));
+    var dadosJson = json.decode( response.body );
+
+    List<Post> postagens = [];
+
+    for( var post in dadosJson ){
+            
+      Post p = Post(
+        userId: post["userId"], 
+        id: post["id"], 
+        title: post["title"], 
+        body: post["body"]);
+
+      postagens.add( p );
+    
+    }
+    return postagens;    
   }
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de serviço avançado"),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: _recuperarPostagens(),
+        builder: (context, snapshot){
 
-    return FutureBuilder<Map>(
-      future: _recuperarPreco(),
-      builder: (context, snapshot){
+          // return  
+          switch( snapshot.connectionState ){
+            case ConnectionState.none :
+            case ConnectionState.waiting :
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.active :
+            case ConnectionState.done :
+              if( snapshot.hasError ){
+                return Center(
+                child: Text('Deu problema.'),
+              );
+              }else {
+                
+                return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                    itemBuilder: (context, index){
 
-        String resultado = '';  
-        double valor = 0;      
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:                                            
-          case ConnectionState.waiting:
-            print('conexão waiting');    
-            resultado = 'Carregando...';        
-            break;
-          case ConnectionState.active:
-          case ConnectionState.done:
-            print('conexão done');            
-            resultado = 'Carregou!';   
-                if (snapshot.hasError) {
-                  resultado = 'Erro na conexão';      
-                } else {
+                      List<Post> lista = snapshot.data!;
+                      Post post = lista[index];
+                      
+                      return ListTile(
+                        title: Text( post.title ),
+                        subtitle: Text( post.id.toString() ),
+                      );
+                    
+                    }
+                );
 
-                  valor = snapshot.data!['BRL']['buy'];
-                  resultado = 'O valor é R\$$valor';
-
-                }            
-        }   
-
-        return Center(
-          child: Text( resultado ),
-        );               
-        
-      }
+              }
+          }
+        },
+      ),
     );
   }
 }
-
---
+++ lib/Post.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, file_names, unnecessary_this, unnecessary_getters_setters
+class Post {
+
+  int userId; 
+  int id;
+  String title;
+  String body;
+
+  Post({required this.userId, required this.id, required this.title, required this.body});
+
+}
